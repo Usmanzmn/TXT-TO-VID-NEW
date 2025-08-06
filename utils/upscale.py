55c7f1b97e6403ddbb18d5e39661da98791525d6
Muhammad Usman <93582993+Usmanzmn@users.noreply.github.com>
2025-08-06 15:30:04 +0500

    Update upscale.py

--- utils/upscale.py
+++ utils/upscale.py
@@ -1,2 +1,7 @@
 from PIL import Image
-...
+
+def upscale_image(image, target_width=720):
+    w, h = image.size
+    scale = target_width / w
+    new_size = (target_width, int(h * scale))
+    return image.resize(new_size, Image.BICUBIC)
