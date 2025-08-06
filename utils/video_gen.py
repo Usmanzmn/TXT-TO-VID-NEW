7cfdddf2a68d104ce783db9f30ecb235f5d55019
Muhammad Usman <93582993+Usmanzmn@users.noreply.github.com>
2025-08-06 14:17:06 +0500

    Delete utils directory

--- utils/video_gen.py
+++
@@ -1,6 +0,0 @@
-from moviepy.editor import ImageSequenceClip
-
-def make_video(image_paths, output_path="outputs/final_video.mp4", fps=1):
-    clip = ImageSequenceClip(image_paths, fps=fps)
-    clip.write_videofile(output_path, codec="libx264")
-    return output_path
